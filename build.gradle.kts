5efb7714300d1ddaade21c5fe71482ee1433f733
bojana <bdimicsurla@gmail.com>
2026-04-06 22:12:40 +0200

    gradle version updated for gradlew

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,13 +19,13 @@ subprojects {
 
     java {
         toolchain {
-            languageVersion.set(JavaLanguageVersion.of(17))
+            languageVersion.set(JavaLanguageVersion.of(21))
         }
         withSourcesJar()
     }
 
     tasks.withType<JavaCompile>().configureEach {
-        options.release.set(17)
+        options.release.set(21)
     }
 
     repositories {
